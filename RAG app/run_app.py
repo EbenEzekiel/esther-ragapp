63100e99bd802ac612e51df4ef8d531df3a37821
Ebenezer O. Ezekiel <122247094+EbenEzekiel@users.noreply.github.com>
2026-02-20 04:47:46 +0100

    Disable Streamlit app launch in run_app.py
    Comment out Streamlit launch code in run_app.py

--- RAG app/run_app.py
+++ RAG app/run_app.py
@@ -43,8 +43,8 @@ def main(mongo_username, mongo_password, mongo_host):
     app_path = Path(__file__).parent / "rag_app" / "application.py"
     #subprocess.run(["streamlit", "run", str(app_path)])
 # Launch Streamlit directly via its CLI module
-    sys.argv = ["streamlit", "run", str(app_path)]
-    sys.exit(stcli.main())
+    #sys.argv = ["streamlit", "run", str(app_path)]
+    #sys.exit(stcli.main())
 
 if __name__ == "__main__":
     main()
